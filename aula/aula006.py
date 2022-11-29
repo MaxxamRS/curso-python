40cdff7ba900df741cee6bcad9b490d13dd893a5
Maximiliano Oliveira Tuchtenhagen <89525237+MaxxamRS@users.noreply.github.com>
2022-11-29 16:08:35 -0300

    add aula

---
+++ aula/aula006.py
@@ -0,0 +1,21 @@
+# n1 = input('Digite um numero')
+# n2 = input('Digite mais um numero')
+# s = n1+n2
+# print('A soma vale', s )
+
+# n1 = int(input('Digite um numero'))
+# n2 = int(input('Digite mais um numero'))
+# s = n1+n2
+# print('A soma vale', s )
+
+
+
+# Primitivo
+# int, 7, -4, 0, 4568
+# float, 4.5, 0.076, -15.546, 7.0
+# bool, true, False
+# str, 'olá', '7.5', ''
+
+# print('Asoma vale',s)
+
+# print('Asoma vale {}',.format(s))
