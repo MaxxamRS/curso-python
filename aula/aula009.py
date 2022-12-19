414ea99f2af8ddca3c1ae93b8132f01ab9d758a5
Maximiliano Oliveira Tuchtenhagen <89525237+MaxxamRS@users.noreply.github.com>
2022-12-19 12:16:58 -0300

    add

---
+++ aula/aula009.py
@@ -0,0 +1,8 @@
+frase = 'Curso em Vídeo em Python'
+# print(frase)
+# print(frase[3])
+# print(frase[2:8])
+# print(frase[:13])
+# print(frase[3::2])
+# print(len(frase))
+# print(frase.replace('Python', 'Android'))
