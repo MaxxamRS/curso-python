26a1e7df4c57bb1d2d754f3e8fdaf6dc1bdd8072
Maximiliano Oliveira Tuchtenhagen <89525237+MaxxamRS@users.noreply.github.com>
2022-12-21 11:42:24 -0300

    Add

--- aula/aula010.py
+++ aula/aula010.py
@@ -1,4 +1,13 @@
-nome = str(input('Qual é o seu nome? '))
+'''nome = str(input('Qual é o seu nome? '))
 if nome == 'Max':
     print('Bom lindo nome você tem! ')
-print('Bom dia {}!'.format(nome))
+print('Bom dia {}!'.format(nome))'''
+
+n1 = float(input('Digite a primeira nota: '))
+n2 = float(input('Digite a segunda nota: '))
+m = (n1 + n2)/2
+print('A sua média foi {:.1f}'.format(m))
+if m >= 6.0:
+    print('Sua media foi boa! PARABÉNS!')
+else:
+    print('Sua média foi ruim! ESTUDE MAIS!')
